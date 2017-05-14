910e4b89ab6901121f5ac6d14e43cb864bbb70bd
Lizi Liao <lizi-git@users.noreply.github.com>
2017-05-14 22:26:40 +0800

    Update SNE.py

--- SNE.py
+++ SNE.py
@@ -3,11 +3,6 @@ Tensorflow implementation of Social Network Embedding framework (SNE)
 
 @author: Lizi Liao (liaolizi.llz@gmail.com)
 
-@references:
-https://github.com/wangz10/tensorflow-playground/blob/master/word2vec.py#L105
-https://www.kaggle.com/c/word2vec-nlp-tutorial/details/
-https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/tutorials/word2vec/word2vec_basic.py
-https://github.com/wangz10/UdacityDeepLearning/blob/master/5_word2vec.ipynb
 '''
 
 
